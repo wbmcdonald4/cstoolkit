6ef9602ef2a79c924f25909530bab5b486b843f7
W Brendan McDonald <wbrendanmcdonald@Ws-MacBook-Pro.local>
2023-05-18 16:11:51 -0400

    add contact page

--- src/main/routes.py
+++ src/main/routes.py
@@ -8,9 +8,9 @@ main = Blueprint('main', __name__)
 def home():
     return render_template("main/home.html", title='Home')
 
-@main.route('/about')
-def about():
-    return render_template("main/about.html", title='About')
+@main.route('/contact')
+def contact():
+    return render_template("main/contact.html", title='contact')
 
 @main.route('/pulse')
 def pulse():
